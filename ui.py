78ee22661ee829b56396f900f79ad294514bffa6
agam_elisha <agamelisha@gmail.com>
2026-09-16 09:31:33 +0300

    ui

--- ui.py
+++ ui.py
@@ -1,18 +1,31 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 #======INITIATE SCREEN======
-root=tk.Tk(screenName="עושים טוב",baseName="עושים טוב",className="Tk",useTk=1)
+root=tk.Tk(screenName="עושים טוב",baseName="עושים טוב",className="Tk",useTk=True)
 root.title("עושים טוב")
+root.configure(bg="#FFD6E8")
 root.geometry("350x600")
+photo = Image.open("logo.png")
+resized_image = photo.resize((350, 300), Image.Resampling.LANCZOS)
+logo=tk_image = ImageTk.PhotoImage(resized_image)
+LOGO_LABEL=tk.Label(root, image=logo,bg="#FFD6E8")
+LOGO_LABEL.place(x=0,y=300)
 
 #========FIRST SCREEN======
-tk.Label(root, text="Login to profile:", font=("Courier", 18)).grid(row=0, column=1)
-tk.Label(root, text="Username:", font=("Ariel", 10)).grid(row=30, column=0)
-tk.Label(root, text="Password:", font=("Ariel", 10)).grid(row=31, column=0)
+title_label=tk.Label(root, text="Login to profile:", font=("Courier", 18),bg="#FFD6E8")
+title_label.place(x=50,y=20)
+
+username_label=tk.Label(root, text="Username:", font=("Ariel", 10),bg="#FFD6E8")
+username_label.place(x=50,y=200)
+
 entry1 = tk.Entry(root)
-entry2 = tk.Entry(root)
-entry1.grid(row=20, column=1)
-entry2.grid(row=21, column=1)
-tk.Button(root, text="Login").grid(row=6, column=1, columnspan=2)
+entry1.place(x=130,y=200)
+
+login_button=tk.Button(root, text="Login")
+login_button.place(x=110,y=230)
+dont_have_user=tk.Label(root, text="Don't Have A user?", font=("Ariel", 10),bg="#FFD6E8")
+creat_user=tk.Button(root, text="signup")
+creat_user.place(x=160,y=230)
 
 
 root.mainloop()
