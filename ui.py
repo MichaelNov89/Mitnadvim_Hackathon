33808de8654182ed35e05a6ef425adb50b9eb9de
agam_elisha <agamelisha@gmail.com>
2026-09-16 12:34:17 +0300

    ui

--- ui.py
+++ ui.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from PIL import Image, ImageTk
-
+import firebasethings as fire
 root=tk.Tk(screenName="עושים טוב",baseName="עושים טוב",className="Tk",useTk=True)
 root.title("עושים טוב")
 root.configure(bg="#85B6C0")
@@ -8,6 +8,14 @@ root.geometry("350x600")
 photo = Image.open("logo.png")
 resized_image = photo.resize((350, 300), Image.Resampling.LANCZOS)
 logo = ImageTk.PhotoImage(resized_image)
+
+def creat_homepage():
+    pass
+
+
+def click(entry):
+    user_text = entry.get()
+    return user_text
 
 
 def signup_screen(login_frame):
@@ -29,7 +37,12 @@ def signup_screen(login_frame):
 
     fill_mail.place(x=150, y=200)
     fill_user_name.place(x=150, y=250)
-    open_account_button=tk.Button(signup_frame ,text="open account",font=("Ariel", 10))
+    username_input = click(fill_user_name)
+    mail_input= click(fill_mail)
+    open_account_button=tk.Button(signup_frame ,text="open account",font=("Ariel", 10),command=lambda: if_user_exist(
+        username_input,mail_input,signup_frame
+    ))
+
     open_account_button.place(x=50,y=280)
     have_an_account=tk.Button(signup_frame, text="already have an account? login", font=("Ariel", 10), command=lambda : back_to_login(
         signup_frame))
@@ -51,10 +64,11 @@ def login_screen():
 
     entry1 = tk.Entry(login_frame)
     entry1.place(x=130, y=200)
+    user_name=click(entry1)
 
-    login_button = tk.Button(login_frame, text="Login")
+    login_button = tk.Button(login_frame, text="Login",command=lambda : fire.sign_in(user_name))
     login_button.place(x=110, y=230)
-    creat_user = tk.Button(login_frame, text="signup", command=lambda: signup_screen(login_frame))
+    creat_user = tk.Button(login_frame, text="signup", command=lambda: if_login(user_name,login_frame))
     creat_user.place(x=160, y=230)
 
 
@@ -63,6 +77,23 @@ def put_logo(frame):
     logo_label = tk.Label(frame, image=logo, bg="#85B6C0")
     logo_label.place(x=0, y=300)
 
+
+
+def if_login(username_input,frame):
+    if fire.sign_in(username_input):
+        frame.destroy()
+        creat_homepage()
+    else:
+        error_label= tk.Label(frame,text="wrong or not existed usrename!",font=("Ariel", 10, "bold"),bg="#85B6C0")
+        error_label.place(x=50,y=260)
+
+def if_user_exist(username_input,mail,frame):
+    if fire.sign_up(username_input,mail):
+        success_label= tk.Label(frame,text="sign up successfully go back to login!")
+        success_label.place(x=50,y=260)
+    else:
+        error_label = tk.Label(frame, text="already registered!", font=("Ariel", 10, "bold"), bg="#85B6C0")
+        error_label.place(x=50, y=260)
 
 
 
